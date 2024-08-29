fe310a5e9d497dda6323c2660d8c1a491345ee63
Karthik Reddy <142746686+8374658910@users.noreply.github.com>
2024-08-29 12:35:43 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
+
 
 
 
